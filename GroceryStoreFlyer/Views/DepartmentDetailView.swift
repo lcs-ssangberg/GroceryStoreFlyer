824b29618ebcc2f056fa5233a0441e7f1054f7a9
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-05 10:27:08 -0500

    organized some views, can navigate and see dept name in detail view

---
+++ GroceryStoreFlyer/Views/DepartmentDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DepartmentDetailView.swift
+//  GroceryStoreFlyer
+//
+//  Created by Sebastian on 2025-02-05.
+//
+
+import SwiftUI
+
+struct DepartmentDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DepartmentDetailView()
+}
