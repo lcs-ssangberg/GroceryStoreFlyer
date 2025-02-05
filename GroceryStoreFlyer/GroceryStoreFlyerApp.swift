9104588d1ffd5577d5efe870c98d85471a55dd51
Sebastian Sangberg <ssangberg@lcs.on.ca>
2025-02-05 09:48:09 -0500

    Initial Commit

---
+++ GroceryStoreFlyer/GroceryStoreFlyerApp.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryStoreFlyerApp.swift
+//  GroceryStoreFlyer
+//
+//  Created by Sebastian on 2025-02-05.
+//
+
+import SwiftUI
+
+@main
+struct GroceryStoreFlyerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
